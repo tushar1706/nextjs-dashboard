ca909d3324b30851c44551496ad33ff65f0dd74d
Tushar Kumar <tkr.1902@gmail.com>
2026-08-14 17:51:43 +0530

    Initial Commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import {Inter,Lusitana} from 'next/font/google';
+export const inter =Inter({subsets:['latin']});
+export const lusitana400=Lusitana({weight:"400",subsets:['latin']})
+export const lusitana700=Lusitana({weight:"700",subsets:['latin']})
+export const lusitana=Lusitana({weight:["400","700"],subsets:['latin']})
