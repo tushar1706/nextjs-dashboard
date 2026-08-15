29a84b16b3ba009c0d3b9db360a123371af75719
Tushar Kumar <tkr.1902@gmail.com>
2026-08-15 13:40:53 +0530

    revenue and invoices fix

--- app/dashboard/(overview) copy/page.tsx
+++ app/dashboard/(overview) copy/page.tsx
@@ -26,8 +26,8 @@ export default async function Page() {
         /> 
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue}  />
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <RevenueChart />
+        <LatestInvoices />
       </div>
     </main>
   );
